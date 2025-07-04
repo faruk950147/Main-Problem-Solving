9ed202da72cd772575068200ab6dc673d65e3087
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-07-04 22:08:26 +0600

    Uploaded all files

---
+++ Javascript/2_chapter/4_string_4/1/operations.js
@@ -0,0 +1,11 @@
+str1 = "Hello World!"
+console.log(str1.length)
+let length = 0;
+
+while (true) {
+    if (str1.charAt(length) == "") {
+        break;
+    }
+    length++;
+}
+console.log(length);

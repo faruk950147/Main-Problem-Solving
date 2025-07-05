5a30ce7c0c04b651fd177c1d70ee54edf922922b
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-07-05 06:05:53 +0600

    Uploaded all files

--- Javascript/2_chapter/4_string_4/1/operations.js
+++ Javascript/2_chapter/4_string_4/1/operations.js
@@ -1,11 +1,21 @@
-str1 = "Hello World!"
-console.log(str1.length)
-let length = 0;
+// str1 = "Hello World!"
+// console.log(str1.length)
+// let length = 0;
 
-while (true) {
-    if (str1.charAt(length) == "") {
-        break;
-    }
-    length++;
+// while (true) {
+//     if (str1.charAt(length) == "") {
+//         break;
+//     }
+//     length++;
+// }
+// console.log(length);
+
+arr = [1, 2, 3, 4, 5];
+console.log(arr.length); // Output: 5
+
+let Length = 0;
+
+while (arr[Length] !== undefined) {
+    Length++;
 }
-console.log(length);
+console.log(Length); // Output: 5

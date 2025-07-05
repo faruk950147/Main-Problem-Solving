e9ac7f17a89f6bc405b0de7a26e2248c7f30b139
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-07-05 15:46:52 +0600

    Uploaded all files

---
+++ Javascript/4_function_4/callback.js
@@ -0,0 +1,19 @@
+function simple(a, b, func) {
+    return func(a, b);
+}
+function add(a, b) {
+    return a + b;
+}
+function sub(a, b) {
+    return a - b;
+}
+function mul(a, b) {
+    return a * b;
+}
+function div(a, b) {
+    return a / b;
+}
+console.log(simple(10, 20, add));
+console.log(simple(10, 20, sub));
+console.log(simple(10, 20, mul));
+console.log(simple(10, 20, div));

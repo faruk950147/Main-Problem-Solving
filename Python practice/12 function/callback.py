175a01ce24706f237454e57cbc384e3284de4b14
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-07-05 15:44:32 +0600

    Uploaded all files

---
+++ Python practice/12 function/callback.py
@@ -0,0 +1,20 @@
+def simple(a, b,func):
+    return func(a, b)
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
+if __name__ == "__main__":
+    print(simple(10, 20, add))
+    print(simple(10, 20, sub))
+    print(simple(10, 20, mul))
+    print(simple(10, 20, div))

bf305794a6b900bcf0317843450fa12bd8a35f66
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-07-05 17:01:42 +0600

    Uploaded all files

---
+++ Javascript/4_function_4/1/some.js
@@ -0,0 +1,26 @@
+// function display(data) {
+//     console.log(data);
+// }
+
+// function calculator(arr) {
+//     let sum = 0;
+//     for(let i=0;i<arr.length;i++) {
+//         sum += arr[i];
+//     }
+//     return sum;
+// }
+// // let result = calculator([1,2,3,4,5]);
+// // display(result);
+
+function some(data) {
+    console.log(data);
+}
+
+function calculator(arr,callback) {
+    let sum = 0;
+    for(let i=0;i<arr.length;i++) {
+        sum += arr[i];
+    }
+    callback(sum);
+}
+calculator([1,2,3,4,5],some);

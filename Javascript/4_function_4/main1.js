d041c5de45a0237d2d494853a0ecf9dde6e8a753
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-07-05 11:38:44 +0600

    Uploaded all files

--- Javascript/4_function_4/main1.js
+++ Javascript/4_function_4/main1.js
@@ -1,16 +1,27 @@
-function test(){
-    for (let i = 0; i < arguments.length; i++) {
-        console.log(arguments[i]);
-    }
-}
-test(1,2,3,4,5);
+// function test(){
+//     for (let i = 0; i < arguments.length; i++) {
+//         console.log(arguments[i]);
+//     }
+// }
+// test(1,2,3,4,5);
 
 
-function add(){
-    sum = 0;
-    for (let i = 0; i < arguments.length; i++) {
-        sum += arguments[i];
+// function add(){
+//     sum = 0;
+//     for (let i = 0; i < arguments.length; i++) {
+//         sum += arguments[i];
+//     }
+//     return sum;
+// }
+// console.log(add(1,2,3,4,5));
+
+function something(greeting, name){
+    function getFirstName(){
+        if (name){
+            return name.split(" ")[0];
+        }
+        return "";
     }
-    return sum;
+    getFirstName();
 }
-console.log(add(1,2,3,4,5));
+something("Hello", "John Doe");
--- Python practice/12 function/nestst function.py
+++ Python practice/12 function/nestst function.py
@@ -1,15 +1,25 @@
-def greet(lang):
-    def eng():
-        return "Hello " + lang
+# def greet(lang):
+#     def eng():
+#         return "Hello " + lang
 
-    def ban():
-        return "Hello " + lang
+#     def ban():
+#         return "Hello " + lang
 
-    if lang == "en":
-        return eng()
-    else:
-        return ban()
+#     if lang == "en":
+#         return eng()
+#     else:
+#         return ban()
 
 
-s = greet("en")
-print(s)
+# s = greet("en")
+# print(s)
+
+
+def something(greeting, name):
+    def getFirstName():
+        if name:
+            return name.split(" ")[0]
+        return ""
+    return greeting + " " + getFirstName()
+
+print(something("Hello", "John Doe"))

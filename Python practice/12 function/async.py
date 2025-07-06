815f42321dc9d93d82b303a464e3f839e4328f13
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-07-06 20:10:16 +0600

    Uploaded all files

--- Python practice/12 function/async.py
+++ Python practice/12 function/async.py
@@ -1,15 +1,35 @@
 import asyncio
+from time import sleep
+# async def Hello_World(word):
+#     await asyncio.sleep(1)
+#     print(word)
 
-async def Hello_World(word):
-    await asyncio.sleep(1)
+# asyncio.run(Hello_World("Hello World"))
+
+# async def main():
+#     await asyncio.gather(
+#         Hello_World("Hello World"),
+#         Hello_World("Hello World"),
+#     )
+
+# asyncio.run(main())
+
+
+async def sayHello(word):
+    sleep(4)
     print(word)
-
-asyncio.run(Hello_World("Hello World"))
 
 async def main():
     await asyncio.gather(
-        Hello_World("Hello World"),
-        Hello_World("Hello World"),
+        sayHello("Hello Line 2"),
     )
 
 asyncio.run(main())
+
+
+print("Hello Line 1")
+asyncio.run(sayHello("Hello Line 2"))
+print("Hello Line 3")
+print("Hello Line 4")
+print("Hello Line 5")
+

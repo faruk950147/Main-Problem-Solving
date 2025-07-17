4e4d94050bfe289ec746a82965b90ce88810da31
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-07-17 12:15:42 +0600

    Uploaded all files

--- Python practice/crud/tasks/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Student(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    phone = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.name

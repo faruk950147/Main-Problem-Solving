4b2aa963c5122853496953a92c7cd23f073c67a6
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-07-18 15:47:48 +0600

    Uploaded all files

---
+++ Python practice/12 function/round.py
@@ -0,0 +1,40 @@
+# Python practice/12 function/round.py
+
+# round function
+# round(number, ndigits)
+# number - number to be rounded
+# ndigits - number of digits after the decimal point
+
+print(round(3.14159, 2)) # output: 3.14
+print(round(3.14159)) # output: 3
+print(round(3.14159, -1)) # output: 3.0
+print(round(3.14159, -2)) # output: 0.0
+print(round(3.14159, -3)) # output: 0.0
+print(round(3.14159, -4)) # output: 0.0
+print(round(3.14159, -5)) # output: 0.0
+print(round(3.14159, -6)) # output: 0.0
+print(round(3.14159, -7)) # output: 0.0
+print(round(3.14159, -8)) # output: 0.0
+print(round(3.14159, -9)) # output: 0.0
+print(round(3.14159, -10)) # output: 0.0
+print(round(3.14159, -11)) # output: 0.0
+print(round(3.14159, -12)) # output: 0.0
+print(round(3.14159, -13)) # output: 0.0
+print(round(3.14159, -14)) # output: 0.0
+print(round(3.14159, -15)) # output: 0.0
+print(round(3.14159, -16)) # output: 0.0
+print(round(3.14159, -17)) # output: 0.0
+print(round(3.14159, -18)) # output: 0.0
+print(round(3.14159, -19)) # output: 0.0
+print(round(3.14159, -20)) # output: 0.0
+print(round(3.14159, -21)) # output: 0.0
+print(round(3.14159, -22)) # output: 0.0
+print(round(3.14159, -23)) # output: 0.0
+print(round(3.14159, -24)) # output: 0.0
+print(round(3.14159, -25)) # output: 0.0
+print(round(3.14159, -26)) # output: 0.0
+print(round(3.14159, -27)) # output: 0.0
+print(round(3.14159, -28)) # output: 0.0
+print(round(3.14159, -29)) # output: 0.0
+print(round(3.14159, -30)) # output: 0.0
+
--- Python practice/qr code/main.py
+++ Python practice/qr code/main.py
@@ -1,13 +1,20 @@
 import qrcode
-"""
-with open("L:\\Programming\\Python\\Main Problem Solving\\Python practice\\qr code\\websites_list.csv", "r") as file:
-    for line in file:
-        data = line.strip().split(",")
-        # print(data[0], data[1])
-        qrcode.make(data[1]).save(f"L:\\Programming\\Python\\Main Problem Solving\\Python practice\\qr code\\qrimg\\{data[0]}.png", scale=10)
-"""
+import time
+
+start_time = time.time()
+# with open("L:\\Programming\\Python\\Main Problem Solving\\Python practice\\qr code\\websites_list.csv", "r") as file:
+#     for line in file:
+#         data = line.strip().split(",")
+#         # print(data[0], data[1])
+#         qrcode.make(data[1]).save(f"L:\\Programming\\Python\\Main Problem Solving\\Python practice\\qr code\\qrimg\\{data[0]}.png", scale=10)
+
+# print("Done in", round(time.time() - start_time, 2), "seconds")
+
+
 with open("websites_list.csv", "r") as file:
     for line in file:
         data = line.strip().split(",")
         # print(data[0], data[1])
-        qrcode.make(data[1]).save(f"qrimg\\{data[0]}.png", scale=10)
+        qrcode.make(data[1]).save(f"qrimg/{data[0]}.png", scale=10)
+
+print("Done in", round(time.time() - start_time, 2), "seconds")

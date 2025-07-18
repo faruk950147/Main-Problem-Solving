77cac23210a9310f73c88bb9b4446a99daa91db6
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-07-18 15:23:20 +0600

    Uploaded all files

--- Python practice/qr code/main.py
+++ Python practice/qr code/main.py
@@ -1,7 +1,13 @@
 import qrcode
-
+"""
 with open("L:\\Programming\\Python\\Main Problem Solving\\Python practice\\qr code\\websites_list.csv", "r") as file:
     for line in file:
         data = line.strip().split(",")
         # print(data[0], data[1])
         qrcode.make(data[1]).save(f"L:\\Programming\\Python\\Main Problem Solving\\Python practice\\qr code\\qrimg\\{data[0]}.png", scale=10)
+"""
+with open("websites_list.csv", "r") as file:
+    for line in file:
+        data = line.strip().split(",")
+        # print(data[0], data[1])
+        qrcode.make(data[1]).save(f"qrimg\\{data[0]}.png", scale=10)

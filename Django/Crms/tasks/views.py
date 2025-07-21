66b0fce040cb93c93126b6caf303ae7640c86f79
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-07-22 00:11:06 +0600

    Uploaded all files

--- Django/Crms/tasks/views.py
+++ Django/Crms/tasks/views.py
@@ -3,7 +3,7 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.cache import never_cache
 from django.contrib.auth import get_user_model
 from django.http import JsonResponse, HttpResponseRedirect, HttpResponse    
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 User = get_user_model()
 import logging
@@ -13,4 +13,8 @@ logger = logging.getLogger(__name__)
 @method_decorator(never_cache, name='dispatch')
 class TasksView(generic.View):
     def get(self, request):
+        try:
+            pass
+        except Exception as e:
+            logger.error(f"{e}")
         return render(request, 'tasks/tasks.html')

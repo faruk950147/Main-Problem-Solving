f5245872e1ef89013c6f4fd74441a4c80eb829d0
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-07-21 23:05:07 +0600

    Uploaded all files

--- Django/Crms/static/assets/js/main.js
+++ Django/Crms/static/assets/js/main.js
@@ -13,3 +13,15 @@ $(document).ready(function () {
 
 });
 
+let profileDropdownList = document.querySelector(".profile-dropdown-list");
+let btn = document.querySelector(".profile-dropdown-btn");
+
+let classList = profileDropdownList.classList;
+
+const toggle = () => classList.toggle("active");
+
+window.addEventListener("click", function (e) {
+  if (!btn.contains(e.target)) classList.remove("active");
+});
+
+

23d9e697a1fe7e817dc5191857e3b14125310c10
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-07-21 23:54:07 +0600

    Uploaded all files

--- Django/Crms/static/assets/js/main.js
+++ Django/Crms/static/assets/js/main.js
@@ -13,15 +13,3 @@ $(document).ready(function () {
 
 });
 
-let profileDropdownList = document.querySelector(".profile-dropdown-list");
-let btn = document.querySelector(".profile-dropdown-btn");
-
-let classList = profileDropdownList.classList;
-
-const toggle = () => classList.toggle("active");
-
-window.addEventListener("click", function (e) {
-  if (!btn.contains(e.target)) classList.remove("active");
-});
-
-

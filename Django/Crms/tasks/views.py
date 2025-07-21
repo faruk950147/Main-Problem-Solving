a2b91261c4bad1b1a2e02c8f5b077e2f8a18f2ba
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-07-21 22:53:32 +0600

    Uploaded all files

--- Django/Crms/tasks/views.py
+++ Django/Crms/tasks/views.py
@@ -1,13 +1,9 @@
 from django.views import generic
-from django.urls import reverse_lazy
-from django.contrib import messages
-from django.shortcuts import render, redirect, get_object_or_404
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import never_cache
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import get_user_model
-from django.http import JsonResponse, HttpResponseRedirect, HttpResponse
-from django.core.mail import EmailMessage
+from django.http import JsonResponse, HttpResponseRedirect, HttpResponse    
+from django.shortcuts import render
 
 User = get_user_model()
 import logging
@@ -17,10 +13,4 @@ logger = logging.getLogger(__name__)
 @method_decorator(never_cache, name='dispatch')
 class TasksView(generic.View):
     def get(self, request):
-        try:
-            pass
-        except Exception as e:
-            logger.error(f"{e}")
-            return redirect('taskview')
-        
         return render(request, 'tasks/tasks.html')

49cd9fe55a5b561f2ede8404784b5f2d60726259
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-07-21 12:11:19 +0600

    Uploaded all files

---
+++ Python practice/31 problems/bulk.py
@@ -0,0 +1,10 @@
+# bulk operation
+# what is bulk operation 
+# when we want to update or delete or insert multiple data at once
+
+def bulk_operation(list):
+    for i in range(len(list)):
+        print(list[i])
+        
+bulk_operation([1,2,3,4,5]) 
+    

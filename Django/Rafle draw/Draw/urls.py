a48cd1ae6edfb522ec21cc632dfff3b80bec2a42
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-07-21 12:28:08 +0600

    Uploaded all files

--- Django/Rafle draw/Draw/urls.py
+++ Django/Rafle draw/Draw/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import RaffleDrawView, AddParticipantView, ParticipantsView     
 
 urlpatterns = [
-    path('', RaffleDrawView.as_view(), name='raffle_draw'),
+    path('', ParticipantsView.as_view(), name='participants'),
+    path('raffle_draw/', RaffleDrawView.as_view(), name='raffle_draw'),
     path('add_participant/', AddParticipantView.as_view(), name='add_participant'),
-    path('participants/', ParticipantsView.as_view(), name='participants'),
     ]

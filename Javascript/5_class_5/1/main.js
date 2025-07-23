1354ef2407339c86743b961e63ab533386fb882d
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-07-23 18:07:13 +0600

    Uploaded all files

--- Javascript/5_class_5/1/main.js
+++ Javascript/5_class_5/1/main.js
@@ -1,12 +1,15 @@
 var rect = {
     width: 10,
     height: 20,
+    // Anonyms function
     draw: function() {
         console.log('Drawing rectangle', this.width, this.height);
     },
+    // Named function anonymous function
     calculateArea: function() {
         return this.width * this.height;
     },
+    // Named function anonymous function
     calculateRange: function() {
         return this.width + this.height;
     }

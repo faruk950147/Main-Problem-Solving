a5847e4a092d4e767fd4012666c794be03924c63
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-07-25 20:05:24 +0600

    Uploaded all files

---
+++ Javascript/4_function_4/2/bind_apply_call.js
@@ -0,0 +1,13 @@
+function display() {
+    // this window
+    console.log(this);
+    console.log("Hello");
+    // custom context bind apply call
+    console.log(this.a + this.b);
+}
+
+// display();
+
+display.bind({a: 10, b: 20})();
+display.apply({a: 10, b: 20});
+display.call({a: 10, b: 20});

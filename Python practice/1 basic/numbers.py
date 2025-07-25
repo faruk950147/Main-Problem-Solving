cfaa3afbc609ccd81a54d0ea84eecc5343cfbd86
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-07-25 11:31:57 +0600

    Uploaded all files

---
+++ Python practice/1 basic/numbers.py
@@ -0,0 +1,25 @@
+# numbers int float complex
+
+# # int
+# num = 10
+# print(type(num))
+
+# # float
+# num = 10.5
+# print(type(num))
+
+# # complex
+# num = 10 + 5j
+# print(type(num))
+
+bin(10)
+print(type(bin(10)))
+print("binary",bin(10))
+
+# oct(10)
+print(type(oct(10)))
+print("octal",oct(10))
+
+# hex(10)
+print(type(hex(10)))
+print("hex",hex(10))

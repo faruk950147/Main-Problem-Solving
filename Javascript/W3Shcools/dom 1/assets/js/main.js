2b53fc2b6e068b80ab80d5e7ddbf37774f8c99f1
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-07-25 21:09:11 +0600

    Uploaded all files

--- Javascript/W3Shcools/dom 1/assets/js/main.js
+++ Javascript/W3Shcools/dom 1/assets/js/main.js
@@ -1,22 +1,63 @@
-// const h1 = document.getElementsByTagName("h1");
-// h1[0].innerHTML = "Hello World!";
+// // select element
+// const heading = document.getElementById("heading");
+// heading.innerHTML = "I'm from javascript!";
+
 // const para = document.getElementsByClassName("para");
-// para[0].innerHTML = "Hello World!";
+// para[0].innerHTML = "I'm from javascript!";
 
-const h1 = document.getElementById("h1");
-h1.innerHTML = "Hello World!";
-const para = document.querySelector(".para");
-para.innerHTML = "Hello World!";
-const btn = document.getElementById("btn");
+// const span = document.getElementsByTagName("span");
+// span[0].innerHTML = "I'm from javascript!";
 
-// console.log(h1);
-// console.log(para);
-// console.log(btn);
-// console.dir(btn);
-// btn.onclick = function () {
-//     alert("Hello World!");
-// };
+// const btn = document.querySelector("#btn");
 
-// btn.addEventListener("click", function () {
-//     alert("Hello World!");
+
+// // apply css
+// heading.style.color = "red";
+// heading.style.backgroundColor = "yellow";
+// heading.style.fontSize = "20px";
+// heading.style.padding = "10px";
+// para[0].style.color = "green";
+// para[0].style.backgroundColor = "yellow";
+// para[0].style.fontSize = "20px";
+// para[0].style.padding = "10px";
+// span[0].style.color = "blue";
+// span[0].style.backgroundColor = "yellow";
+// span[0].style.fontSize = "20px";
+// span[0].style.padding = "10px";
+
+// btn.addEventListener("click", () => {
+//     console.log("clicked");
+//     heading.style.color = "blue";
+//     para[0].style.color = "blue";
+//     span[0].style.color = "blue";
 // });
+
+//// add attribute
+// heading.setAttribute("class", "heading");
+// para[0].setAttribute("class", "para");
+// span[0].setAttribute("class", "span");
+
+// // remove attribute
+// heading.removeAttribute("class");
+// para[0].removeAttribute("class");
+// span[0].removeAttribute("class");
+
+// // get attribute
+// console.log(heading.getAttribute("class"));
+// console.log(para[0].getAttribute("class"));
+// console.log(span[0].getAttribute("class"));
+
+// // get text content
+// console.log(heading.textContent);
+// console.log(para[0].textContent);
+// console.log(span[0].textContent);   
+demo = document.getElementById("demo");
+const form = document.forms["form"];
+let text = "";
+for (let i = 0; i < form.length; i++) {
+    text += form.elements[i].value + "\n";
+}
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    demo.innerHTML = text;
+});

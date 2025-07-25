abf35ad2d174253668250607f0b3d51e6de5ce3d
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-07-25 09:55:45 +0600

    Uploaded all files

---
+++ Javascript/4_function_4/anonymous.js
@@ -0,0 +1,15 @@
+// anonymous function is a function without name
+
+// named function i
+function add(a, b) {
+    return a + b;
+}
+
+console.log(add(2, 3));
+
+// anonymous function
+const add = function(a, b) {
+    return a + b;
+}
+
+console.log(add(2, 3));
---
+++ Python practice/12 function/anonymous.py
@@ -0,0 +1,9 @@
+# anonymous function is a function without name function
+# lambda function is a anonymous function
+
+# lambda function is a one line function
+# lambda function is a anonymous function
+
+add = lambda a, b: a + b
+print(add(10, 20))
+

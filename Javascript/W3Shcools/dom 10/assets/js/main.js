1cf44295a13c8c851fa93864c90501c4461e0612
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-07-26 10:28:53 +0600

    Uploaded all files

--- Javascript/W3Shcools/dom 10/assets/js/main.js
+++ Javascript/W3Shcools/dom 10/assets/js/main.js
@@ -1,22 +1,13 @@
-// const h1 = document.getElementsByTagName("h1");
-// h1[0].innerHTML = "Hello World!";
-// const para = document.getElementsByClassName("para");
-// para[0].innerHTML = "Hello World!";
-
-const h1 = document.getElementById("h1");
-h1.innerHTML = "Hello World!";
-const para = document.querySelector(".para");
-para.innerHTML = "Hello World!";
+const table = document.querySelector(".table");
 const btn = document.getElementById("btn");
-
-// console.log(h1);
-// console.log(para);
-// console.log(btn);
-// console.dir(btn);
-// btn.onclick = function () {
-//     alert("Hello World!");
-// };
-
-// btn.addEventListener("click", function () {
-//     alert("Hello World!");
-// });
+btn.addEventListener("click", function () {
+    fetch("assets/js/data.json")
+        .then((response) => response.json())
+        .then((data) => {
+            table.innerHTML += `<tr>
+                <td>${data[0].name}</td>
+                <td>${data[0].age}</td>
+                <td>${data[0].city}</td>
+            </tr>`;
+        });
+});

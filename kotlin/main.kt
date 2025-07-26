1c62e7e11848aa5c16358546b546531686231950
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-07-26 21:23:07 +0600

    Uploaded all files

--- kotlin/main.kt
+++ kotlin/main.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
-    println(args)
+    println(args.joinToString()) // show the args-of value 
     println("Hello Kotlin Programming!")
     println("Hello Kotlin Bangladesh!")
     println("Hello Kotlin Android!")

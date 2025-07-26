dee6331a22e31ddc97b87e41062fb270181656b9
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-07-26 06:48:58 +0600

    Uploaded all files

---
+++ Javascript/W3Shcools/dom 3/assets/js/main.js
@@ -0,0 +1,42 @@
+// // navigator properties
+// // 1. parentNode
+// let p = document.getElementById("demo").parentNode;
+// // console.log(p);
+// // 2. parentElement
+// let pe = document.getElementById("demo").parentElement;
+// console.log(pe);
+// // 3. childNodes
+// let cn = document.getElementById("demo").childNodes;
+// console.log(cn);
+// // 4. children
+// let ch = document.getElementById("demo").children;
+// console.log(ch);
+// // 5. childElement
+// let ce = document.getElementById("demo").childElement;
+// console.log(ce);
+// // 5. firstChild
+// let fc = document.getElementById("demo").firstChild;
+// console.log(fc);
+// // 6. lastChild
+// let lc = document.getElementById("demo").lastChild;
+// console.log(lc);
+// // 7. nextSibling
+// let ns = document.getElementById("demo").nextSibling;
+// console.log(ns);
+// // 8. previousSibling
+// let ps = document.getElementById("demo").previousSibling;
+// console.log(ps);
+// // 9. nodeValue;
+// const demo = document.getElementById("demo");
+// // way one
+// console.log(demo.innerHTML);
+// // demo.innerHTML = "Hello World!";
+// // way two
+// console.log(demo.textContent);
+// // demo.textContent = "Hello World!";
+// // way three
+// console.log(demo.innerText);
+// // demo.innerText = "Hello World!";
+// // way four
+// const way_four = demo.firstChild.nodeValue;
+// console.log(way_four);

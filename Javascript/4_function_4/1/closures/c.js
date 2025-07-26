4464a0e318ac2591811e05f440c640b320a8ba28
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-07-26 12:27:05 +0600

    Uploaded all files

---
+++ Javascript/4_function_4/1/closures/c.js
@@ -0,0 +1,13 @@
+// normal function
+// function add(a, b) {
+//     return a + b;
+// }
+// console.log(add(2, 3));
+// Scope global
+num1 = 10;
+num2 = 20;
+
+function add(){
+    return(num1 + num2);
+}
+console.log(add());

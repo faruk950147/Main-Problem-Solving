14589683a868d2b130afcc32510a62a434b5eab0
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-07-26 10:08:21 +0600

    Uploaded all files

--- Javascript/4_function_4/async_await.js
+++ Javascript/4_function_4/async_await.js
@@ -1,9 +1,50 @@
 let paymentSuccess = true;
 let marks = 80;
 
-async function display() {
-    console.log("Displaying...");
+function enroll() {
+    console.log("Enrolling...");
+    const promise = new Promise(function(resolve, reject) {
+        if (paymentSuccess) {
+            resolve("Payment successful");
+        } else {
+            reject("Payment failed");
+        }
+    });
+    return promise;
 }
 
-display();
+function progress() {
+    console.log("Processing...");
+    const promise = new Promise(function(resolve, reject) {
+        if (marks >= 80) {
+            resolve("You are eligible for the certificate");
+        } else {
+            reject("You are not eligible for the certificate");
+        }
+    });
+    return promise;
+}
+
+function getCertificate() {
+    console.log("Getting certificate...");
+    const promise = new Promise(function(resolve, reject) {
+        if (marks >= 80) {
+            resolve("You are eligible for the certificate");
+        } else {
+            reject("You are not eligible for the certificate");
+        }
+    });
+    return promise;
+}
+
+async function courseFlow() {
+    try {
+        await enroll();
+        await progress();
+        await getCertificate();
+    } catch (err) {
+        console.log(err);
+    }
+}
+courseFlow();
 

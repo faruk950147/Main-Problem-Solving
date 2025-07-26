f8e478f667afde9f71fe46d89b0fae50c4028f94
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-07-26 10:49:06 +0600

    Uploaded all files

--- Python practice/28 json/json3.py
+++ Python practice/28 json/json3.py
@@ -1,4 +1,13 @@
-import json 
+import json
+# what is json is a data format 
+# json is a data format that is used to store and exchange data
+# json is language independent and platform independent
+# json is a text format that is written in key-value pairs
+# absolutely double quote is used in json
+
+
+# dumps is used to write json data to a file
+# loads is used to read json data from a file
 
 products = {
     "products": [

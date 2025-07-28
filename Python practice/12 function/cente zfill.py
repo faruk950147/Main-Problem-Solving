8b12e613f8ca830aec010fa1525e93304bc136fd
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-07-28 14:01:43 +0600

    Uploaded all files

---
+++ Python practice/12 function/cente zfill.py
@@ -0,0 +1,18 @@
+# center() and zfill() in Python
+
+# center() in Python
+# center() is a string method
+# center() is used to center align the string
+# center() is used to center align the string
+
+# zfill() in Python
+# zfill() is a string method
+# zfill() is used to fill the string with zeros
+# zfill() is used to fill the string with zeros
+
+str1 = "Hello"
+str2 = "World"
+print(str1.center(10))
+print(str2.center(10))
+print(str1.zfill(10))
+print(str2.zfill(10))

be37afefe879255d08938c4c77a0ed590319f4b5
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-07-31 02:14:12 +0600

    Uploaded all files

--- Django/RestFramework/tasks/urls.py
+++ Django/RestFramework/tasks/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from tasks.views import TaskListView
 urlpatterns = [
-    path('tasks/', TaskListView.as_view(), name='task-list'),
+    path('', TaskListView.as_view(), name='task-list'),
 ]

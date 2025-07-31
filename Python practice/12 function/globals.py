15ce04d22f44a82dd3c6c6a484eb97d878322942
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-07-31 18:50:34 +0600

    Uploaded all files

--- Python practice/12 function/globals.py
+++ Python practice/12 function/globals.py
@@ -1,1 +1,11 @@
 num = 10
+def display():
+    # it is not allowed to modify global variable num += 10
+    # it is allowed to modify global keyword num that is global num
+    global num
+    num += 10
+    print("num in function global",num)
+    num = 20
+    print("num in function local",num)
+display()
+print("num in outside global",num)

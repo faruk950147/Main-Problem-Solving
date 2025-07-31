a81e8e5bf5409f5d326b86fa15126f6e52d28cbf
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-07-31 21:59:18 +0600

    Uploaded all files

--- Python practice/12 function/lamda/lamda.py
+++ Python practice/12 function/lamda/lamda.py
@@ -21,14 +21,31 @@
 # sortedData = lambda data: sorted(data)
 # print(sortedData([5, 3, 1]))
 
-from ast import arguments
+# from ast import arguments
 
 
-people = [['John', 'Doe', '25', 'M'], ['Jane', 'Smith', '22', 'F']]
+# people = [['John', 'Doe', '25', 'M'], ['Jane', 'Smith', '22', 'F']]
 # Syntax arguments: expression
 # arguments is receive data
 # expression is a return value
 # arguments is data
 # expression is sorted(data, key=lambda person: int(person[2]))
-sorted_people = lambda data: sorted(data, key=lambda person: int(person[2]))
-print(sorted_people(people))
+# sorted_people = lambda data: sorted(data, key=lambda person: int(person[2]))
+# print(sorted_people(people))
+
+# def strsorted(data):
+#     return sorted(data, key=lambda person: int(person[2]))
+# print(strsorted([['John', 'Doe', '25', 'M'], ['Jane', 'Smith', '22', 'F']]))    
+
+# Using Lambda with the sorted() Function:
+
+def secondWord(name):
+    return name.split()[1] # name of the second word
+
+data = ['Sharma Rohit', 'Kohli Virat', 'Tendulkar Sachin', 
+        'Raina Suresh', 'Gill Shubman', 'Kishan Ishan']
+
+# key secondWord function
+print(sorted(data, key=secondWord))
+
+

c3e87d9f3d95e56da04c6360fdbadcf1f234a4b0
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-08-01 08:22:26 +0600

    Uploaded all files

--- Python practice/12 function/recursion/Recursion.py
+++ Python practice/12 function/recursion/Recursion.py
@@ -1,4 +1,5 @@
-
+# Recursion is a function that calls itself again and again
+# By default, Python has a limit on the number of recursive calls it can make (recursion limit) 1000
 def recursive_sum(n):
     if n == 1:
         return 1
--- Python practice/12 function/recursion/recursion1.py
+++ Python practice/12 function/recursion/recursion1.py
@@ -1,1 +1,2 @@
 # Recursion is a function that calls itself again and again
+# By default, Python has a limit on the number of recursive calls it can make (recursion limit) 1000
